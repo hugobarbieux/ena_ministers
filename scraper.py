c2564475a256df535db2a543f410a446152de053
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2019-04-24 10:52:15 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ html = scraperwiki.scrape("https://www.linkedin.com/search/results/people/?compa
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-root.cssselect("div h3 span['class=name actor-name']")
+root.cssselect("div h3 span")
 # matchedlinks = root.cssselect
 
 #
